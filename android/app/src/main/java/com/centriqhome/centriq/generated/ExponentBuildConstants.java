da537ac2baca9f6c8ffd79bbf59d331e56cf5c39
Dom Kiva-Meyer <git@domkm.com>
2017-10-30 17:12:36 -0400

    Detach

---
+++ android/app/src/main/java/com/centriqhome/centriq/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.centriqhome.centriq.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp20089b3d1be7456c91f58e5f73c5f4bf://ce-rx4.centriq.auth0-example.exp.direct:80";
+
+}
